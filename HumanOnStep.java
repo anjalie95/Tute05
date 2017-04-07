6716b40580813cd61bd5708aa125f593a73532e1
anjalie95 <anjaleemendis9522@gmail.com>
2017-04-07 10:01:46 +0530

    Create HumanOnStep.java

---
+++ HumanOnStep.java
@@ -0,0 +1,22 @@
+/*
+Q3)
+Write another function to print following structure using standard output of java.
+    0    *
+   /|\   *
+    |    *
+   / \   *
+*  *  *  *
+*/
+class HumanOnStep{
+	public static int printhumanonstep(){
+		System.out.println("    0    *");
+		System.out.println("   /|\\   *");
+		System.out.println("    |    *");
+		System.out.println("   / \\   *");
+		System.out.println("*  *  *  *");
+    return 0;
+	}
+	public static void main(String args[]){
+		printhumanonstep();
+	}
+}
